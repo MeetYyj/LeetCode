c16dc711831abd4296538da6c085f2cfcc949633
meetyyj <meetyyj@gmail.com>
2018-04-19 21:35:41 +0800

    387

--- cpp_code/387-First-Unique-Character-in-a-String /main.cpp
+++ cpp_code/387-First-Unique-Character-in-a-String /main.cpp
@@ -2,35 +2,55 @@
 #include <vector>
 using namespace std;
 
-class msg{
- public:
-  int cnt = 0;
-  int idx = -1;
-};
+//class msg{
+// public:
+//  int cnt = 0;
+//  int idx = -1;
+//};
+//
+//class Solution {
+// public:
+//  int firstUniqChar(string s) {
+//    vector<msg> cvec(26);
+//    for (int i = 0; i < s.size(); ++i) {
+//      cvec[s[i] - 'a'].cnt++;
+//      if(cvec[s[i] - 'a'].cnt == 1){
+//        cvec[s[i] - 'a'].idx = i;
+//      }
+//    }
+//    int min_idx = -1;
+//    for (int j = 0; j < 26; ++j) {
+//      if(cvec[j].cnt == 1){
+//        if(min_idx > cvec[j].idx || min_idx == -1){
+//          min_idx = cvec[j].idx;
+//        }
+//      }
+//    }
+//
+//    return min_idx;
+//
+//  }
+//};
 
 class Solution {
  public:
   int firstUniqChar(string s) {
-    vector<msg> cvec(26);
-    for (int i = 0; i < s.size(); ++i) {
-      cvec[s[i] - 'a'].cnt++;
-      if(cvec[s[i] - 'a'].cnt == 1){
-        cvec[s[i] - 'a'].idx = i;
-      }
+    vector<int> cnts(128, 0);
+    for(auto c : s) {
+      ++cnts[c];
     }
-    for (int j = 0; j < 26; ++j) {
-      if(cvec[j].cnt == 1){
-        return cvec[j].idx;
+    for(int i = 0; i < s.size(); ++i) {
+      if (cnts[s[i]] == 1) {
+        return i;
       }
     }
     return -1;
-
   }
 };
 
 
 int main() {
   Solution s;
-  cout << s.firstUniqChar("leetcode");
+  cout << s.firstUniqChar("aa");
   return 0;
 }
